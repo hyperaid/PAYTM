b0474f07c10c1c109087a24dd99630bc50784200
hyperaid <abhinavsharma2k19@gmail.com>
2024-05-20 16:01:22 +0530

    created all pages and setupped tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
  import './App.css'
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import Dashboard from './pages/Dashboard'
+import SendMoney from './pages/SendMoney'
+
  
 function App() {
 
+
   return (
     <>
-    <h1> hellllo</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+      </Routes>
+    </BrowserRouter>
+     </>
   )
 }
 
